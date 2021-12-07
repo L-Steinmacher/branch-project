4cbd3c6ff5b96ecf55e03173e1bd888ba6eae83a
L-Steinmacher <dasai.sacul@gmail.com>
2021-12-07 12:23:45 -0800

    create branch method writen and ready to be tested

--- LambdaFunctions/branchHandler.py
+++ LambdaFunctions/branchHandler.py
@@ -1,3 +1,4 @@
+import re
 import psycopg2
 import os
 from dotenv import load_dotenv
@@ -14,19 +15,64 @@ db_pass=os.getenv("DB_PASS")
 
 conn = psycopg2.connect(dbname=db_name, user=db_user, password=db_pass, host=db_host)
 
-GET_RAW_PATH = "/getBranch"
-CREATE_RAW_PATH = "/createBranch"
+GET_BRANCH_RAW_PATH = "/getBranch"
+CREATE_BRANCH_RAW_PATH = "/createBranch"
 
 def branch_handler(event, context):
     cur = conn.cursor()
     print(event)
+    decodedEvent = json.loads(event['body'])
 
-    if event['rawPath'] == GET_RAW_PATH:
+    if event['rawPath'] == GET_BRANCH_RAW_PATH:
         # access DB and grab branch with the ID that was passed
-        pass
-    elif event['rawPath'] == CREATE_RAW_PATH:
+        id = decodedEvent['branchId']
+        get_branch(id)
+
+    elif event['rawPath'] == CREATE_BRANCH_RAW_PATH:
         # insert into DB a new branch
-        pass
+        brId = decodedEvent['branchId']
+        crmId = decodedEvent['crmId']
+        create_branch(brId, crmId)
+
+def get_branch(id):
+    sql = """Select * FROM branches
+              WHERE branchId = (s%);"""
+    responseObj = {}
+
+    try:
+        #Connect to DB
+        cur = conn.cursor()
+        cur.execute(sql,id)
+        branch = cur.fetchone()
+        conn.commit()
+        cur.close()
+
+    except (Exception, psycopg2.DatabaseError) as e:
+        print('Unable to connect!\n{0}').format(e)
+
+def create_branch(branch_id, crm_id):
+    sql = """INSERT INTO branches(branchId, crmId)
+             VALUES(s%, s%) RETURNING branchId;"""
+    transactionResponse = {}
+    responseObj = {}
+
+    try:
+        # Connect to the DB
+        cur = conn.cursor()
+        # inserting into database a new branch
+        cur.execute(sql, (branch_id, crm_id))
+        transactionResponse['branchId'] = branch_id
+        transactionResponse['crmId'] = crm_id
+        transactionResponse['message'] = "created"
+        responseObj['statusCode'] = 200
+        responseObj['headers'] = {}
+        responseObj['headers']['Content-Type'] = 'application/json'
+        responseObj['body'] = transactionResponse
+
+        return responseObj
+
+    except (Exception, psycopg2.DatabaseError) as e:
+        print('Unable to connect!\n{0}').format(e)
 
 
-branch_handler()
+
